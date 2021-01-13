61c3fb148a89fe89912b0876c765cd29ada27ad6
hadi-cyber <hadikusumanto2000@gmail.com>
2021-01-13 11:22:42 +0700

    UAS DWH

--- uas_dwh.sql
+++ uas_dwh.sql
@@ -19,7 +19,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `uas_dwh`
+-- Database: `data_warehouse`
 --
 
 -- --------------------------------------------------------
